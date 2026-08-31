d294a5fad340145cdf63015ea3132a581b19f7c6
Fudge Breadstein <trubhawkins@gmail.com>
2026-08-31 19:44:20 -0400

    adjustedt oColor member change:

--- main.c
+++ main.c
@@ -136,9 +136,9 @@ void *renderForms(void *data) {
 		};
 		
 		if (f->id == 1) {
-			pc.color.vals[1] = 64;
+			pc.color.rgb[1] = 64;
 		} else if (f->id == 2) {
-			pc.color.vals[2] = 64;
+			pc.color.rgb[2] = 64;
 		}
 		memcpy(reco.data, &pc, sizeof(PosColor));
 	}
